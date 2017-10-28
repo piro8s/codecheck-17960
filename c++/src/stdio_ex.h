8a0a1ae931b6ffa8e8ba969ce46f51e14222f16d
Yasuhiro Kawazoe <se7en.8s.piro@gmain.com>
2017-10-28 20:58:52 +0900

    色々修正

--- c++/src/stdio_ex.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-/**
- * Reset buffer on stdin stream.
- */
-void flush(void);
-
-int Log(const char *, const char *);
--- c++/src/time_ex.c
+++
@@ -1,5 +0,0 @@
-#include "time_ex.h"
-
-int roundSecToHour(time_t t) {
-	return t/60/60;
-}
