13f86354354f99958cd564e0433c8096b0d020a9
Yasuhiro Kawazoe <se7en.8s.piro@gmain.com>
2017-10-29 15:12:34 +0900

    try

--- c++/src/main.c
+++ c++/src/main.c
@@ -6,32 +6,38 @@
 #define WAITING_SEC 3
 
 int main(int argc, char *argv[]) {
-	char *in_ym = (char *)calloc(TARGET_YEAR_MONTH_CHR_LEN+1, sizeof(char));
+	// char *in_ym = (char *)calloc(TARGET_YEAR_MONTH_CHR_LEN+1, sizeof(char));
 	TotalWorkHours *total = (TotalWorkHours *)malloc(sizeof(TotalWorkHours));
 	int i=0, j=0;
 
 	//Target Year-Month
-	scanf("%7[0-9/]", in_ym);
-	flush();
-	// int initTWH = initTotalWorkHours(argv[1], total);
-	int initTWH = initTotalWorkHours(in_ym, total);
+	// scanf("%7[0-9/]", in_ym);
+	// flush();
+
+	char *del;
+	printf("argv[1]:%s\n", argv[1]);
+	printf("strlen:%d\n", (int)strlen(argv[1]));
+	printf("delch():%d\n", delch(argv[1], '/', del, strlen(argv[1])));
+	printf("delch:%s\n", del);
+	int initTWH = initTotalWorkHours(argv[1], total);
+	// int initTWH = initTotalWorkHours(in_ym, total);
 	if (initTWH != SUCCESS) return initTWH;
 
 //*
-	while(1) {
-	// for (i=2; i<=argc; i++) {
-		char *in_wh = (char *)calloc(WORKING_HOUR_PERIOD_CHR_LEN+1, sizeof(char));
+	// while(1) {
+	for (i=2; i<=argc; i++) {
+		// char *in_wh = (char *)calloc(WORKING_HOUR_PERIOD_CHR_LEN+1, sizeof(char));
 		DailyWorkHours *daily = (DailyWorkHours *)malloc(sizeof(DailyWorkHours));
 		static time_t temp_weeklyWH = (time_t)0;
 		static int lastWorkDay = 0;
 		static int lastWorkWeekday = 7;
 
-		int flg =scanfWithTimeout("%60[ 0-9/:-]", in_wh, WAITING_SEC);
-		if (in_wh[0] == '0') break;
-		flush();
+		// int flg =scanfWithTimeout("%60[ 0-9/:-]", in_wh, WAITING_SEC);
+		// if (in_wh[0] == '0') break;
+		// flush();
 
-		// int initDWH = initDailyWorkHours(argv[i], daily);
-		int initDWH = initDailyWorkHours(in_wh, daily);
+		int initDWH = initDailyWorkHours(argv[i], daily);
+		// int initDWH = initDailyWorkHours(in_wh, daily);
 		if (initDWH == END) break;
 		else if (initDWH != SUCCESS) return initDWH;
 		if (isWorkingOnSameWeek(daily, lastWorkDay) == FALSE) temp_weeklyWH = (time_t)0;
@@ -49,8 +55,8 @@ int main(int argc, char *argv[]) {
 		else if (culcWH == ERROR_P) return 920;
 
 		free(daily);
-		free(in_wh);
-		i++;
+		// free(in_wh);
+		// i++;
 	}
 //*/
 
@@ -61,7 +67,7 @@ int main(int argc, char *argv[]) {
 	printf("%d\n", roundSecToHour(total->nonlegalHolydayWH));
 	printf("%d\n", roundSecToHour(total->legalHolydayWH));
 
-	free(total);
+	// free(total);
 	return 0;
 }
 
