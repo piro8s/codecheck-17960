bbadb438597bded6f4676ff351903497beb1509a
Yasuhiro Kawazoe <se7en.8s.piro@gmain.com>
2017-10-29 15:15:00 +0900

    try

--- c++/src/main.c
+++ c++/src/main.c
@@ -14,16 +14,17 @@ int main(int argc, char *argv[]) {
 	// scanf("%7[0-9/]", in_ym);
 	// flush();
 
-	char *del;
+	char *del; del = (char *)calloc(TARGET_YEAR_MONTH_CHR_LEN, sizeof(char));
 	printf("argv[1]:%s\n", argv[1]);
 	printf("strlen:%d\n", (int)strlen(argv[1]));
 	printf("delch():%d\n", delch(argv[1], '/', del, strlen(argv[1])));
 	printf("delch:%s\n", del);
+/*
 	int initTWH = initTotalWorkHours(argv[1], total);
 	// int initTWH = initTotalWorkHours(in_ym, total);
 	if (initTWH != SUCCESS) return initTWH;
-
-//*
+//*/
+/*
 	// while(1) {
 	for (i=2; i<=argc; i++) {
 		// char *in_wh = (char *)calloc(WORKING_HOUR_PERIOD_CHR_LEN+1, sizeof(char));
